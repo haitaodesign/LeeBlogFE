7a1a985e8ccab479dbe47e668c0d9fd3194d1218
lihaitao <1775814590@qq.com>
2018-04-25 16:20:46 +0800

    test

--- client/main.js
+++ client/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-const root = document.createElement('div');
-document.body.appendChild(root)
-
 import './assets/images/bg.jpeg'
 import './assets/styles/test.scss'
 
+const root = document.createElement('div')
+document.body.appendChild(root)
+
 new Vue({
-    render: (h) => h(App)
+  render: (h) => h(App)  
 }).$mount(root)
