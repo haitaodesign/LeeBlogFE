26d02aa8fb1d075d52b362d030049e5b470cdf72
lihaitao <1775814590@qq.com>
2018-04-30 10:37:56 +0800

    add favicon.icon

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
-
+const send = require('koa-send')
+const path = require('path')
 const app = new Koa()
 const pageRouter = require('./routers/dev-ssr')
 const isDev = process.env.NODE_ENV === 'development'
@@ -18,7 +19,13 @@ app.use(async (ctx, next) => {
     }
   }
 })
-
+app.use(async (ctx, next) => {
+  if (ctx.path === '/favicon.ico') {
+    await send(ctx, '/favicon.ico', {root: path.join(__dirname, '../')})
+  } else {
+    await next()
+  }
+})
 app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
 
 const HOST = process.env.HOST || '0.0.0.0'
