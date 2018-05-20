95fa9d7a1fea4e99f4b2972230402811a5291848
lihaitao <1775814590@qq.com>
2018-05-20 15:33:36 +0800

    add card

---
+++ client/components/Card/card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lee-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card'
+  }
+</script>
+
+<style lang="stylus" scoped>
+.lee-card
+  margin 16px
+  border 1px solid #dddee1
+  border-radius 3px
+  background-color #fff
+  &:hover
+    box-shadow:#dddee1 0 0 10px ;
+</style>
+
