9785b50f39b90328e59d7c41065ed808f33d7e3a
lihaitao <15393243613@163.com>
2018-12-04 21:45:10 +0800

    add footer

--- client/views/index/index.vue
+++ client/views/index/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="lee-layout">
-    <div class="lee-layout-top">
+    <header class="lee-layout-top">
       <lee-header></lee-header>
-    </div>
-    <div class="lee-layout-main">
+    </header>
+    <main class="lee-layout-main">
         <router-view></router-view>
-    </div>
+    </main>
+    <footer class="lee-layout-footer">
+      <div>© 2018 leehaitao.com</div>
+    </footer>
   </div>
 </template>
 
@@ -35,6 +38,8 @@ export default {
   margin 0 auto
   width 75%
   height 100%
+.lee-layout-footer
+  text-align center
 @media screen and (max-width: 960px)
    .lee-layout-main
      width 100%
