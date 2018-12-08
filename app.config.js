e07d5afadcb432d906b098b144bdd6509c8e761c
lihaitao <15393243613@163.com>
2018-12-08 22:18:09 +0800

    delete 删除敏感信息配置文件

--- app.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  cdn: {
-    host: 'http://cdn.leehaitao.com/',
-    bucket: 'leeblogfe',
-    ak: '_EUicTCRIGMeEVRHCnU7FsvXNmy5TJTciIDk-JFl',
-    sk: 'UWWaFn4S5t3WfNwrOdqcA3YmWbRbCKgLDIiHkfYX'
-  }
-}
