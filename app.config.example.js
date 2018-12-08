fad252494147f274a22adbcb4385f2d8db05a5de
lihaitao <15393243613@163.com>
2018-12-08 23:09:13 +0800

    update 修改配置文件示例文件

--- app.config.example.js
+++ app.config.example.js
@@ -1,8 +1,9 @@
 module.exports = {
-  cdn: {
-    host: '',
-    bucket: '',
-    ak: '',
-    sk: ''
+  acm: {
+    endpoint: 'acm.aliyun.com', // Available in the ACM console
+    namespace: '', // Available in the ACM console
+    accessKey: '', // Available in the ACM console
+    secretKey: '', // Available in the ACM console
+    requestTimeout: 6000 // Request timeout, 6s by default
   }
 }
