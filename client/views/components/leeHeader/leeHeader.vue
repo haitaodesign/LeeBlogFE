cc206dbbe417ac8dac5acd12b1585bacd00700cf
haitaodesign <15393243613@163.com>
2018-12-15 19:14:12 +0800

    update 重构leeheader组件，使用iview menu组件替代

--- client/views/components/leeHeader/leeHeader.vue
+++ client/views/components/leeHeader/leeHeader.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="lee-header clearfix">
-    <div class="lee-header-title">
-      leeHaitao
+    <div class="lee-header-logo" @click="handleLogoClick">
+      leehiatao.com
     </div>
-    <div class="lee-header-right clearfix">
-      <ul class="lee-header-nav clearfix">
-        <li class="lee-header-item" v-for="item in navlist" :key="item.id" @click="handleOnNavItemClick(item)">{{item.name}}</li>
-      </ul>
+    <div class="lee-header-nav">
+      <Menu mode="horizontal" theme="primary" active-name="文章" @on-select="handleOnSelectClick">
+         <MenuItem :name="item.name" v-for="item in navlist" :key="item.id">
+            {{item.name}}
+        </MenuItem>
+      </Menu>
     </div>
   </div>
 </template>
@@ -25,37 +27,33 @@
       }
     },
     methods: {
-      handleOnNavItemClick (e) {
-        const name = e.name
+      handleOnSelectClick (name) {
         if (name === '文章') {
           this.$router.push({path: '/articles'})
         } else if (name === '关于') {
           this.$Message.info('开发中。。。')
         }
+      },
+      handleLogoClick () {
+        window.location.reload()
       }
     }
   }
 </script>
 
 <style lang="stylus" scoped>
-@import '../../../assets/styles/variable.styl';
+@import '../../../assets/styles/variable.styl'
 .lee-header
-  color #fff
-  .lee-header-title
+  width 1080px
+  margin 0 auto
+  .lee-header-logo
+    padding 0 10px
     float left
-    width 120px
-    text-align center
-  .lee-header-right
-    float right
-    .lee-header-nav
-      text-align center
-      .lee-header-item
-        float left
-        height 100%
-        width 80px
-        list-style-type none
-        cursor pointer
-        &:hover
-          background-color $secondary
+    color #fff
+    font-size 18px
+    cursor pointer
+  .lee-header-nav
+    margin-left 20px
+    float left
 </style>
 
